9d1463ea01c84b44b3e983b75ad94252aa842e69
Chris Bessent <lost.dragonist@gmail.com>
2022-01-23 04:33:58 -0700

    Update version to 1.6.0

--- src/gameskyrim.cpp
+++ src/gameskyrim.cpp
@@ -99,7 +99,7 @@ QString GameSkyrim::description() const
 
 MOBase::VersionInfo GameSkyrim::version() const
 {
-  return VersionInfo(1, 5, 0, VersionInfo::RELEASE_FINAL);
+  return VersionInfo(1, 6, 0, VersionInfo::RELEASE_FINAL);
 }
 
 QList<PluginSetting> GameSkyrim::settings() const
